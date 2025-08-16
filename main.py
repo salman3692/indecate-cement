582aa6ec278235192c85ce5bc04cbc64dfb61668
salman3692 <m.salman@uliege.be>
2025-08-16 19:09:43 +0200

    modified:   main.py 	modified:   pareto-frontend/src/App.jsx

--- main.py
+++ main.py
@@ -36,8 +36,7 @@ config_list = [
 # Load surrogate models
 models = {}
 for config_name in config_list:
-    # file_path = MODELS_DIR / f"surrogate_{config_name}.pkl"
-    file_path = MODELS_DIR / f"surrogate_{config_name}_linux.pkl"
+    file_path = MODELS_DIR / f"surrogate_{config_name}.pkl"
     if file_path.exists():
         try:
             model = joblib.load(file_path)
